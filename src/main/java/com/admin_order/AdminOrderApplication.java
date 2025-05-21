57e4b120432d12badebeb693d80044068a119b4a
Binuka <it24101538@my.sliit.lk>
2025-05-21 13:27:59 +0530

    Initial commit for Order Processing Component

---
+++ src/main/java/com/admin_order/AdminOrderApplication.java
@@ -0,0 +1,13 @@
+package com.admin_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminOrderApplication.class, args);
+	}
+
+}
